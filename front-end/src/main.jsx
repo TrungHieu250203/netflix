dd98f3e3f472753cc01adea2c6f43990d71d2407
unknown <trunghieu20032502@gmail.com>
2024-06-17 10:33:59 +0700

    first commit

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,18 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
-import GlobalStyles from "../src/components/GlobalStyles";
+import "./GlobalStyles.module.scss";
 import { MovieProvider } from "./context/movie-provider";
 import { NotificationProvider } from "./components/socket/socket.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobalStyles>
-      <NotificationProvider>
-        <MovieProvider>
-          <App />
-        </MovieProvider>
-      </NotificationProvider>
-    </GlobalStyles>
+    <NotificationProvider>
+      <MovieProvider>
+        <App />
+      </MovieProvider>
+    </NotificationProvider>
   </React.StrictMode>
 );
