ee04c2937a93a21d4f8968ba051c132e9a4c0193
unknown <trunghieu20032502@gmail.com>
2024-06-17 09:50:14 +0700

    first commit

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteSass } from "vite-plugin-sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSass()],
+  plugins: [react()],
   assetsInclude: ["**/*.m4v"],
 });
